f49a4eddde1efe4850a2892f50d44192a318655e
ChanneDesign <channebertrand@gmail.com>
2016-01-10 09:10:05 -0500

    some changes

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,10 +1,59 @@
 $(document).ready(function(){
-  $('.owl-carousel').owlCarousel({
-  	items: 1,
-  	loop: true,
-  	autoplay: true,
-  	autoplayTimeout: 3000,
-  	autoplayHoverPause: true,
+	$('.owl-carousel').owlCarousel({
+	  	items: 1,
+	  	loop: true,
+	  	autoplay: true,
+	  	autoplayTimeout: 3000,
+	  	autoplayHoverPause: true,
+	  	margin:10
 
-  });
+	 });
+
+		
+	   
+  	
+	$(window).scroll(function() {   
+
+	    var photoBackground = $('.photo_background').offset().top;
+	    var homeMainContent = $('.home_main_content').offset().top;
+	    var nav = $(".navbar");
+
+	    
+		//alert(photoBackground);
+	    if (homeMainContent < (photoBackground + 450)) { 
+	        nav.css({ opacity: 1 });
+	    } else {
+			nav.css({ opacity: 0 });
+		};
+
+		if (photoBackground > 450) {
+			$('.home_main_content').addClass("margin_home_main_content");
+			nav.addClass("navbar-fixed-top");
+			nav.removeClass("margin_nav");
+		} else {
+			nav.removeClass("navbar-fixed-top");
+			nav.addClass("margin_nav");
+			$('.home_main_content').removeClass("margin_home_main_content");
+		}
+	});
+
+	//SmoothScrolling
+	$(function() {
+	  $('.smooth_scroll').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+		  var target = $(this.hash);
+		  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		  if (target.length) {
+			$('html,body').animate({
+			  scrollTop: target.offset().top -50
+			}, 1000);
+			return false;
+		  }
+		}
+	  });
+	});
+
+	
+
 });
+
