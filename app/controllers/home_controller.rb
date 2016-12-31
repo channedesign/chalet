ec370d30bb3378cca49202ba67d3f0d2a3746f26
ChanneDesign <channebertrand@gmail.com>
2016-12-31 11:59:11 -0500

    add pictures to view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
 
 	def index
-		@chalet1_appartments = House.first.appartments
-		@chalet2_appartments = House.last.appartments
+		@chalet1 = House.first
+		@chalet2 = House.last
 		@prices = PriceListing.all
 		@prices2 = PriceListingTwo.all
 	end
